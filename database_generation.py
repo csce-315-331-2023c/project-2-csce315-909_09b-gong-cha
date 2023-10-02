0f6afd6b94b869f40e36317fbea8d38438f6ab95
AntonAsus <noki.ramirez@gmail.com>
2023-10-02 13:44:42 -0500

    Added more changes to database generation script

--- database_generation.py
+++ database_generation.py
@@ -7,19 +7,20 @@ recipes = pd.read_csv('recipes.csv')
 x = pd.date_range(end='10/2/2023', periods=365, freq = 'D')
 print(x)
 
-file = open("Orders.csv", "w")
-
-# features: order ID, order item, order price
 
 # distribution of orders: 100 a day, 200 on peak days
 
 # peak days are saturday, sundays
 
-# each order has (1, 5) items
+# each order has [1, 5] items
 
-# each order item has 
+# each order item has [0, 2] toppings
 
+file = open("Order_Item.csv", "w")
 
+# Order_Item.csv features: Order_Item_ID, Order_ID, Recipe_ID, Notes, Is_Medium, Ice, Sugar, Item_Price
+
+# Order_Toppings.csv features: Order_Item_ID, Topping_Id
 
 for i in x:
     print(i.month, i.day, i.year)
