165d3b37d30189ba2d719d5eaad780037a445d20
AntonAsus <noki.ramirez@gmail.com>
2023-10-02 10:06:49 -0500

    Adjusted Database

--- database_generation.py
+++ database_generation.py
@@ -1,4 +1,28 @@
 # script used to generate database of 365 days of sales
 import pandas as pd
+import numpy as np
+
+recipes = pd.read_csv('recipes.csv')
+
+x = pd.date_range(end='10/2/2023', periods=365, freq = 'D')
+print(x)
+
+file = open("Orders.csv", "w")
+
+# features: order ID, order item, order price
+
+# distribution of orders: 100 a day, 200 on peak days
+
+# peak days are saturday, sundays
+
+# each order has (1, 5) items
+
+
+
+
+
+for i in x:
+    print(i.month, i.day, i.year)
+
 
 
