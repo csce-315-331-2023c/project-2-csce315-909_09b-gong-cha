6e60c70bf3578768245db65963954fc2cf1d468b
AntonAsus <noki.ramirez@gmail.com>
2023-10-15 16:54:26 -0500

    application

--- src/main/java/com/example/gongchapos/Application.java
+++ src/main/java/com/example/gongchapos/Application.java
@@ -1137,4 +1137,105 @@ public class Application {
     return toReturn;
    }
 
+
+
+
+  // given 2 dates and a menu item, show all orders that inlcude that menu item between those 2 dates
+  /**
+   * returns object[][] of order_item_id, order_id, notes, is_medium, ice, sugar, price
+   * @param init_time
+   * @param final_time
+   * @param menu_item
+   * @return object[][] 
+   */
+  public Object[][] getSalesReport(String init_time, String final_time, String menu_item){
+
+    ArrayList<String> order_ids = new ArrayList<String>();
+
+
+    int main_recipe_id = 0;
+
+
+    try
+    {
+      Statement stmt = conn.createStatement();
+      ResultSet result = stmt.executeQuery("SELECT * FROM recipe;");
+      while(result.next())
+      {
+        if (result.getString("recipe_name").equals(menu_item)){
+          main_recipe_id = result.getInt("recipe_id");
+        }
+      }
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(null, "Error accessing Database");
+    }
+    System.out.println("main recipe id" +main_recipe_id);
+
+
+    // obtain list of order_id in between time frame
+    // iterate through each order_id, and select from order_item (using order_id) where recipe_id = recipe_id of menu_item
+    // sql: SELECT * FROM order_ WHERE date_ BETWEEN '2022-10-03' AND '2022-10-04' AND time_ BETWEEN '14:00' AND '15:00';
+    // add onto object[][] array
+
+    try{
+      Statement stmt = conn.createStatement();
+      ResultSet result = stmt.executeQuery("SELECT * FROM order_ WHERE date_ BETWEEN '"+ init_time + "' AND '" + final_time + "';");
+      while(result.next())
+      {
+        String temp_id = String.valueOf(result.getInt("order_id"));
+        order_ids.add(temp_id);
+      }
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(null, "Error accessing Database");
+    }
+
+    ArrayList<ArrayList<String>> tempContainer = new ArrayList<ArrayList<String>>();
+    for (String order_id: order_ids){
+      try{
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery("SELECT * FROM order_item where order_id = '"+ order_id + "' and recipe_id = '" + main_recipe_id + "';");
+        if(result.isBeforeFirst())
+        {
+          result.next();
+          ArrayList<String> cur_drink = new ArrayList<String>();
+          String order_item_id = String.valueOf(result.getInt("order_item_id"));
+          String notes = result.getString("notes");
+          String is_medium = String.valueOf(result.getBoolean("is_medium"));
+          String ice = result.getString("ice");
+          String sugar = result.getString("sugar");
+          String price = String.valueOf(result.getDouble("item_price"));
+          cur_drink.add(order_item_id);
+          cur_drink.add(order_id);
+          cur_drink.add(notes);
+          cur_drink.add(is_medium);
+          cur_drink.add(ice);
+          cur_drink.add(sugar);
+          cur_drink.add(price);
+          tempContainer.add(cur_drink);
+        }
+      } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Error accessing Database");
+      }
+    
+    }
+    Object[][] toReturn = new Object[tempContainer.size()][7];
+    for(int i = 0; i < tempContainer.size(); i++){
+      ArrayList<String> cur_arr = tempContainer.get(i);
+      Object[] cur = new Object[6];
+      cur = cur_arr.toArray();
+      toReturn[i] = cur;
+    }
+    // for(int i = 0; i  < tempContainer.size(); i++){
+    //   ArrayList<String> cur_arr = tempContainer.get(i);
+    //   for(int j = 0; j < cur_arr.size(); j++){
+    //     System.out.print(cur_arr.get(j)+ ", ");
+    //   }
+    //   System.out.println();
+    // }
+
+    return toReturn;  
+  }
+
+
+
 }
